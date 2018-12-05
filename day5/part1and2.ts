ac7d574d77b335e2c4cefec95cddce8a42d06a4f
James Humphries <james@james-humphries.co.uk>
2018-12-05 23:58:44 +0000

    added performance

--- day5/part1and2.ts
+++ day5/part1and2.ts
@@ -1,24 +1,28 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import { performance } from 'perf_hooks';
 
 const input = fs.readFileSync(path.resolve(__dirname, 'input.txt')).toString();
 
 export function react(polymer: string) {
     let i = 0;
     let units = polymer.split('');
-    while (units[i + 1] !== undefined) {
+    while (i + 1 < units.length) {
         if (i < 0) { i = 0; }
         if ((units[i].charCodeAt(0) ^ units[i + 1].charCodeAt(0)) === 32) {
-            units.splice(i, 2);
-            i -= 2;
+            units.splice(i, 2); i -= 2;
         }
         i++;
     }
-    return units.join('');;
+    return units.join('');
 }
 
+let start = performance.now();
 console.log(`Part 1 Solution : ${react(input).length}`);
+let end = performance.now();
+console.log(`Solved in ${end - start}ms`);
 
+start = performance.now();
 let result = Math.min(...'abcdefghijklmnopqrstuvwxyz'
     .split('') //foreach letter of the alphabet
     .map((u) => input.split('').filter((unit) => unit.toLowerCase() !== u).join('')) //remove the letter from input
@@ -27,3 +31,5 @@ let result = Math.min(...'abcdefghijklmnopqrstuvwxyz'
 );
 
 console.log(`Part 2 Solution: ${result}`);
+end = performance.now();
+console.log(`Solved in ${end - start}ms`);
