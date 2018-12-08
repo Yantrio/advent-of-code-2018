9f0f738963b2f0e3092e7aa4318d6b7604676b7c
James Humphries <james@james-humphries.co.uk>
2018-12-08 11:24:54 +0000

    added tests too

--- day8/part1and2.ts
+++ day8/part1and2.ts
@@ -1,9 +1,6 @@
 import { input } from './input.json';
 
-export type Node = {
-    id: number, childCount: number, metadataCount: number, children: Node[], metadata: number[]
-}
-
+type Node = { id: number, childCount: number, metadataCount: number, children: Node[], metadata: number[] }
 const sum = (numbers: number[]) => numbers.reduce((a, b) => a + b, 0);
 
 const getNodeValue = (node: Node) => (node.childCount === 0) ?
@@ -22,6 +19,18 @@ function parse(input: number[], id: number = 0, nodeList: Node[] = []): { parsed
     return { parsed, nodeList };
 }
 
-const { parsed: rootNode, nodeList } = parse([...input]);
-console.log(`Solution Pt 1: ${sum(nodeList.map(((n) => sum(n.metadata))))}`);
-console.log(`Solution Pt 2: ${getNodeValue(rootNode)}`);
+function solve(input: number[]) {
+    const { parsed, nodeList } = parse([...input]);
+    return { pt1: sum(nodeList.map(((n) => sum(n.metadata)))), pt2: getNodeValue(parsed) };
+}
+
+// tests  
+const testInput = [2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2];
+let { pt1: pt1Test, pt2: pt2Test } = solve(testInput);
+console.log(`Test Solution Pt 1: ${pt1Test} [Expected 138]}`);
+console.log(`Test Solution Pt 2: ${pt2Test} [Expected 66]`);
+
+// solution
+let { pt1, pt2 } = solve(input);
+console.log(`Solution Pt 1: ${pt1}`);
+console.log(`Solution Pt 2: ${pt2}`);
