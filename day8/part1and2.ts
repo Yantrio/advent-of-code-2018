42b0e93251b5c1a45d4c8aa0ce8ca4e4a600d8c5
James Humphries <james@james-humphries.co.uk>
2018-12-08 11:09:27 +0000

    added default values

--- day8/part1and2.ts
+++ day8/part1and2.ts
@@ -8,11 +8,10 @@ const sum = (numbers: number[]) => numbers.reduce((a, b) => a + b, 0);
 
 const getNodeValue = (node: Node) => (node.childCount === 0) ?
     sum(node.metadata) :
-    sum(node.metadata
-        .filter((i) => i <= node.children.length)
+    sum(node.metadata.filter((i) => i <= node.children.length)
         .map((idx) => getNodeValue(node.children[idx - 1])));
 
-function parse(input: number[], id: number, nodes: Node[]): { node: Node, nodeList: Node[] } {
+function parse(input: number[], id: number = 0, nodes: Node[] = []): { node: Node, nodeList: Node[] } {
     const [childCount, metadataCount] = [input.shift(), input.shift()];
     const parsed: Node = {
         id: id++, childCount, metadataCount,
@@ -23,6 +22,6 @@ function parse(input: number[], id: number, nodes: Node[]): { node: Node, nodeLi
     return { node: parsed, nodeList: nodes };
 }
 
-const { node: rootNode, nodeList } = parse([...input], 0, []);
+const { node: rootNode, nodeList } = parse([...input]);
 console.log(`Solution Pt 1: ${sum(nodeList.map(((n) => sum(n.metadata))))}`);
 console.log(`Solution Pt 2: ${getNodeValue(rootNode)}`);
